4101c4a395641e8d656ef1caca7052feed0ce0f2
ChenghaoCai <784847190@qq.com>
2016-07-23 13:45:13 +0100

    Add files via upload

--- example_unblocking.pl
+++ example_unblocking.pl
@@ -1,7 +1,7 @@
 % The example 9.3 on pp.34-35 of the paper.
 
 % USAGE: In SWI-Prolog, run:
-% notrace,[diagnose,repair,util,reform,revise,utilRevise,filter,extralib,example_unblocking].
+% notrace,[diagnose,repair,util,reform,revise,utilRevise,extralib,example_unblocking].
 % and run:
 % example_9_3.
 
@@ -10,7 +10,7 @@ example_9_3:-
   % The upper limit of the cost of repair.
   Nr = 10,
 
-  % Filter. See filter.pl.
+  % Filter. See filter1 in this file.
   filter1(F),
 
   % Y is a goal. Ax is an axiom.
@@ -39,4 +39,45 @@ example_9_3:-
   % Output the result.
   nl,printByLine(['Repairs are:'|Reps]),
   nl,printByLine(['The repaired axiom is:'|[Ax1]]),nl.
+
+
+
+% Filters can prohibit some repairs when unblocking unification.
+% To enable/prohibit a repair, you could comment/uncomment it, as some examples below.
+
+% The filter of example_9_3.
+filter1(F):-
+  F = 
+  [
+    % Prohibit merging.
+    % merge(_,_,left) ,
+    merge(_,_,right) ,
+
+    % Prohibit adding new arguments (constants).
+    % addargc(_,_,_,_,left) ,
+    % addargc(_,_,_,_,right) ,
+
+    % Prohibit adding new arguments (variables).
+    % addargv(_,_,_,left) ,
+    % addargv(_,_,_,right) ,
+
+    % Prohibit deleting arguments.
+    % delarg(_,_,_,left) ,
+    % delarg(_,_,_,right) ,
+
+    % Prohibit adding new functors.
+    addfunc(_,_,_,left) ,
+    addfunc(_,_,_,right) ,
+
+    % Prohibit deleting functors.
+    delfunc(_,_,_,left) ,
+    delfunc(_,_,_,right) ,
+    
+    % Prohibit permuting arguments.
+    permute(_,_,left) ,
+    permute(_,_,right) ,
+
+    tail_of_filter
+  ].
+
 
--- util.pl
+++ util.pl
@@ -337,3 +337,77 @@ filter(Rs,Ftr):-
   member(X,Rs),
   member(X,Ftr).
 
+% Filters can prohibit some repairs when unblocking unification.
+% To enable/prohibit a repair, you could comment/uncomment it, as some examples below.
+
+% A filter for repairing rewriting rules.
+filter_rewrite(F):-
+  F = 
+  [
+    % Prohibit merging.
+    % merge(_,_,left) ,
+    merge(_,_,right) ,
+
+    % Prohibit adding new arguments (constants).
+    % addargc(_,_,_,_,left) ,
+    addargc(_,_,_,_,right) ,
+
+    % Prohibit adding new arguments (variables).
+    % addargv(_,_,_,left) ,
+    addargv(_,_,_,right) ,
+
+    % Prohibit deleting arguments.
+    delarg(_,_,_,left) ,
+    delarg(_,_,_,right) ,
+
+    % Prohibit adding new functors.
+    % addfunc(_,_,_,left) ,
+    addfunc(_,_,_,right) ,
+
+    % Prohibit deleting functors.
+    delfunc(_,_,_,left) ,
+    delfunc(_,_,_,right) ,
+    
+    % Prohibit permuting arguments.
+    % permute(_,_,left) ,
+    permute(_,_,right) ,
+
+    tail_of_filter
+  ].
+
+
+% The default filter.
+filter_default(F):-
+  F = 
+  [
+    % Prohibit merging.
+    % merge(_,_,left) ,
+    merge(_,_,right) ,
+
+    % Prohibit adding new arguments (constants).
+    % addargc(_,_,_,_,left) ,
+    addargc(_,_,_,_,right) ,
+
+    % Prohibit adding new arguments (variables).
+    % addargv(_,_,_,left) ,
+    addargv(_,_,_,right) ,
+
+    % Prohibit deleting arguments.
+    delarg(_,_,_,left) ,
+    delarg(_,_,_,right) ,
+
+    % Prohibit adding new functors.
+    addfunc(_,_,_,left) ,
+    addfunc(_,_,_,right) ,
+
+    % Prohibit deleting functors.
+    delfunc(_,_,_,left) ,
+    delfunc(_,_,_,right) ,
+    
+    % Prohibit permuting arguments.
+    permute(_,_,left) ,
+    permute(_,_,right) ,
+
+    tail_of_filter
+  ].
+
